92e0960919a4c1d93c7c8373ecfd11cc126457c3
mk1994 <murugeshk252@gmail.com>
2026-02-17 17:21:44 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-});
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
